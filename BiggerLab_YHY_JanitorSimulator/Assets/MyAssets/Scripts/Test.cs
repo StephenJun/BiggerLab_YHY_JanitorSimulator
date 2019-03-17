4dca7a0a7d10cd955fe3ea266a846d0d2d24f786
StephenJun <StephenJun@users.github.com>
2019-03-17 12:33:32 +0800

    init commit

---
+++ BiggerLab_YHY_JanitorSimulator/Assets/MyAssets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        PrintTest("Peter", 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // transform.Rotate(Vector3.up);
+    }
+
+
+
+    public void PrintTest(string name)
+    {
+        print(name);
+    }
+    public void PrintTest()
+    {
+        print("YHY");
+    }
+    public void PrintTest(string name, int num)
+    {
+        print(name + num.ToString());
+    }
+}
